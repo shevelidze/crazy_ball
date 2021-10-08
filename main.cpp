e533ea23e8b35580e89ee54ea4d7856ddc9b9bf3
Denys Shevel <denys.shevel16@gmail.com>
2021-10-08 23:31:00 +0300

    Add moving view and new background

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ class CrazyBallApp : public App
 private:
     sf::Texture background_texture;
     sf::Sprite background_sprite;
+    dj::MovebleSprite *focusSprite;
+    sf::FloatRect viewRect = sf::FloatRect(0, 0, 1600, 1200);
 
 public:
     CrazyBallApp(sf::RenderWindow *window)
@@ -45,12 +47,13 @@ public:
             floor->move(1200 + i * 80, 600);
             this->staticSprites.push_back(floor);
         }
-        this->movebleSprites.push_back(new dj::MainBallSprite);
+        this->focusSprite = new dj::MainBallSprite;
+        this->movebleSprites.push_back(focusSprite);
         this->background_texture.loadFromFile("./images/background.png");
         this->background_texture.setRepeated(true);
         this->background_sprite.setTexture(this->background_texture);
-
-        this->background_sprite.setTextureRect(sf::IntRect(0, 0, 10000, 10000));
+        this->background_sprite.setTextureRect(sf::IntRect(0, 0, 100000, 100000));
+        this->background_sprite.move(-this->background_sprite.getGlobalBounds().width / 2, -this->background_sprite.getGlobalBounds().height / 2);
     }
     const sf::RenderWindow &get_window()
     {
@@ -78,13 +81,28 @@ public:
             animationIterator->tick(eventsVector, *this);
             this->window->draw(*animationIterator);
         }
+        sf::FloatRect focusSpriteBounds = this->focusSprite->getGlobalBounds();
+        float gaps = 100;
+        float leftDistance = focusSpriteBounds.left - this->viewRect.left;
+        float rightDistance = this->viewRect.left + this->viewRect.width - focusSpriteBounds.left - focusSpriteBounds.width;
+        if (rightDistance < gaps)
+        {
+            this->viewRect.left += gaps - rightDistance;
+        }
+        if (leftDistance < gaps)
+        {
+            this->viewRect.left -= gaps - leftDistance;
+        }
+        sf::View newView = this->window->getView();
+        newView.reset(this->viewRect);
+        this->window->setView(newView);
         this->window->display();
     }
 };
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
     CrazyBallApp app(&window);
     while (window.isOpen())
     {
